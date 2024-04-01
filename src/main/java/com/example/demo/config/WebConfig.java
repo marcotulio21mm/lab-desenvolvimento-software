d9e318cfc95b92187a8aef815d30046409d4acbd
marcotulio21mm <marcotulio21mm@gmail.com>
2024-03-31 23:48:35 -0300

    alterações estrutura

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // ou lista de domínios permitidos explicitamente
+                .allowedOriginPatterns("*") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
---
+++ src/main/java/com/example/demo/service/TaskService.java
@@ -0,0 +1,68 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.Task;
+import com.example.demo.Repository.TaskRepository;
+
+@Service
+public class TaskService {
+    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public ResponseEntity<List<Task>> listAll() {
+        try {
+            List<Task> taskList = new ArrayList<Task>();
+            taskRepository.findAll().forEach(taskList::add);
+            if (taskList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(taskList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public Task createTask(String tittle) {
+        Task task = new Task(tittle);
+        task.setTittle(tittle);
+        task.setStatus("pending");
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(long id, String tittle) {
+        Task task = taskRepository.findById(id).orElse(null);
+        if (task != null) {
+            task.setTittle(tittle);
+            return taskRepository.save(task);
+        }
+        return null;
+    }
+
+    public Task concludeTask(Long id) {
+        Task task = taskRepository.findById(id).orElse(null);
+        if (task != null) {
+            task.setStatus("completed");
+            return taskRepository.save(task);
+        }
+        return null;
+    }
+
+    public void deleteById(Long id) {
+        Task task = taskRepository.findById(id).orElse(null);
+        if (task != null) {
+            taskRepository.deleteById(id);
+        }
+    }
+
+    public List<Task> getAll() {
+        return taskRepository.findAll();
+    }
+}
