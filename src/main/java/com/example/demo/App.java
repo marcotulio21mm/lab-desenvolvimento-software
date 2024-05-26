af6d4871e92c8b2d591c6a863ee9254906a90f8d
marcotulio21mm <marcotulio21mm@gmail.com>
2024-05-26 15:29:39 -0300

    mudando nome do application pra corrigir erro

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class DemoApplication {
+@ComponentScan(basePackages = "com.example.demo.Controller,com.example.demo.service")
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
